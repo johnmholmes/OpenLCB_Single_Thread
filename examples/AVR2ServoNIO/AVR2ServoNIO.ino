97237f936b86dda7daf382b2781204366d9ca541
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2024-11-30 14:03:33 +0000

    Update AVR2ServoNIO.ino

--- examples/AVR2ServoNIO/AVR2ServoNIO.ino
+++ examples/AVR2ServoNIO/AVR2ServoNIO.ino
@@ -381,8 +381,7 @@ void userConfigWritten(uint32_t address, uint16_t length, uint16_t func)
 
 // Reinitialize servos to their current positions
 // Called from setup() and after every configuration change
-/*
-    void servoSetup() {
+void servoSetup() {
   servodelay = NODECONFIG.read( EEADDR(servodelay));
   PV(servodelay);
   for(uint8_t i = 0; i < NUM_SERVOS; i++) {
@@ -391,21 +390,21 @@ void userConfigWritten(uint32_t address, uint16_t length, uint16_t func)
     servoTarget[i] = NODECONFIG.read( EEADDR(servos[i].pos[cpos].angle) );
   }
 }
-*/
 
-void servoSetup() {
-  servodelay = NODECONFIG.read(EEADDR(servodelay));
-  PV(servodelay);
-  for (uint8_t i = 0; i < NUM_SERVOS; i++) {
-    uint8_t cpos = NODECONFIG.read(EEADDR(curpos[i])); 
-    // Attach the servo to the specified pin
-    servo[i].attach(servopin[i]); 
-    // Set servo target to 90 degrees at startup
-    servoTarget[i] = 90;
-    // Move the servo to the initial position
-    servo[i].write(servoTarget[i]);
-  }
-}
+
+// void servoSetup() {
+//   servodelay = NODECONFIG.read(EEADDR(servodelay));
+//   PV(servodelay);
+//   for (uint8_t i = 0; i < NUM_SERVOS; i++) {
+//     uint8_t cpos = NODECONFIG.read(EEADDR(curpos[i])); 
+//     // Attach the servo to the specified pin
+//     servo[i].attach(servopin[i]); 
+//     // Set servo target to 90 degrees at startup
+//     servoTarget[i] = 90;
+//     // Move the servo to the initial position
+//     servo[i].write(servoTarget[i]);
+//   }
+// }
 
 // Setup the io pins
 // called by setup() and after a configuration change
